163f19f7dec223806652c766bc29f51bcc5a5a61
Bex Birkett <bex.birkett@outlook.com>
2020-05-28 16:21:00 +0100

    finished Header.js & Header.css, added folder w/ font Lato & styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,13 @@
 import React from "react"
-import { FaSearch } from 'react-icons/fa';
+import { IoIosSearch } from "react-icons/io";
+import './Header.css'
 
 const Header = (props) => {
     return(
         <header>
             <img src={props.imageWizard} alt = {"channel icon"} />
             <h1>The Wizard's SQUAD</h1>
-            <FaSearch />
+            <IoIosSearch />
         </header>
     )
 }
