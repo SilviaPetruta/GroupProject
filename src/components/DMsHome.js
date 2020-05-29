df2aba74bcb86c9283200e2270c3309a9b7bfeca
SilviaPetruta <silvia_petruta@yahoo.com>
2020-05-29 14:00:48 +0100

    css for ChannelsHome and DMsHome

--- src/components/DMsHome.js
+++ src/components/DMsHome.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import './ChannelsAndDMsHome.css';
 
 import { FaCircle } from 'react-icons/fa';
+import { FaHeart } from 'react-icons/fa';
+import { FaEdit } from 'react-icons/fa';
+import { FaPlus } from 'react-icons/fa';
 
 const DMsHome = (props) => {
-    return (
-        <div>
-            <div>
-                <FaCircle />
-                <p>{props.userOnline}</p>
+    const dmsHome = props.users.map(input => {
+        return (
+            <div key={input.id} className='dm'>
+                <FaCircle className='iconCircle' />
+                <p>{input.userOn}</p>
             </div>
-            
+        )
+    });
+
+    return (
+        <div className='DmsDiv'>
+            <div>
+                <h4>Direct messages</h4>
+                <FaPlus className='iconPlus' />
+            </div>
+			<div className='dm'>
+                <FaHeart className='iconHeart' />
+                <p>Slackbot</p>
+            </div>
+            {dmsHome}
+            <FaEdit className='iconEdit' />
         </div>
     )
 }
